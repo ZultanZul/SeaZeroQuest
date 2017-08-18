86daf0ab08e8d72ff5be9ddf081fc31801862c62
Chris Halliwell <chris.halliwel@saatchi.co.uk>
2017-08-18 12:36:33 +0100

    amend: Sea shader alpha

--- Level2/js/script.js
+++ Level2/js/script.js
@@ -16,7 +16,7 @@ var Colors = {
 };
 
 var vertexShader = `
-#define SCALE 10.0
+#define SCALE 30.0
 
 varying vec2 vUv;
 
@@ -24,8 +24,8 @@ uniform float uTime;
 
 float calculateSurface(float x, float z) {
     float y = 0.0;
-    y += (sin(x * 1.0 / SCALE + uTime * 1.0) + sin(x * 2.3 / SCALE + uTime * 1.5) + sin(x * 3.3 / SCALE + uTime * 0.4)) / 3.0;
-    y += (sin(z * 0.2 / SCALE + uTime * 1.8) + sin(z * 1.8 / SCALE + uTime * 1.8) + sin(z * 2.8 / SCALE + uTime * 0.8)) / 3.0;
+    y += sin(x * 1.3 / SCALE + uTime * 0.8);
+    y += sin(z * 1.7 / SCALE + uTime * 1.0);
     return y;
 }
 
@@ -35,7 +35,7 @@ void main() {
     
     float strength = 1.0;
     pos.y += strength * calculateSurface(pos.x, pos.z);
-    pos.y -= strength * calculateSurface(0.0, 0.0);
+    pos.y -= strength * calculateSurface(0.0, 0.5);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }  
@@ -49,18 +49,19 @@ uniform float uTime;
 uniform vec3 uColor;
 
 void main() {
-    vec2 uv = vUv * 10.0 + vec2(uTime * -0.05);
+    vec2 uv = vUv * 30.0 + vec2(uTime * -0.05);
 
     uv.y += 0.01 * (sin(uv.x * 3.5 + uTime * 0.35) + sin(uv.x * 4.8 + uTime * 1.05) + sin(uv.x * 7.3 + uTime * 0.45)) / 3.0;
     uv.x += 0.12 * (sin(uv.y * 4.0 + uTime * 0.5) + sin(uv.y * 6.8 + uTime * 0.75) + sin(uv.y * 11.3 + uTime * 0.2)) / 3.0;
     uv.y += 0.12 * (sin(uv.x * 4.2 + uTime * 0.64) + sin(uv.x * 6.3 + uTime * 1.65) + sin(uv.x * 8.2 + uTime * 0.45)) / 3.0;
 
     vec4 tex1 = texture2D(uMap, uv * 1.0);
-    vec4 tex2 = texture2D(uMap, uv * 1.0 + vec2(0.2));
 
     vec3 blue = uColor;
 
-    gl_FragColor = vec4(blue + vec3(tex1.a * 0.9 - tex2.a * 0.02), 1.0);
+    gl_FragColor = vec4(blue + vec3(tex1.a * 0.65), 1.0);
+    gl_FragColor.a = 0.8;
+
 }
 `;
 
@@ -149,7 +150,11 @@ var Sea = function() {
 	this.uniforms = {
         uMap: {type: 't', value: null},
         uTime: {type: 'f', value: 0},
-        uColor: {type: 'f', value: new THREE.Color('#0051da')},
+        uColor: {type: 'f', value: new THREE.Color('#307ddd')},
+	    fogColor:    { type: "c", value: scene.fog.color },
+	    fogNear:     { type: "f", value: scene.fog.near },
+	    fogDensity: 1.0,
+	    fogFar:      { type: "f", value: scene.fog.far },
         fog: true
     };
 
@@ -158,13 +163,13 @@ var Sea = function() {
         vertexShader: vertexShader,
         fragmentShader: fragmentShader,
         side: THREE.DoubleSide,
+        transparent:true
     });
 
     var textureLoader = new THREE.TextureLoader();
     textureLoader.load('images/water-shader.png', function (texture) {
         shader.uniforms.uMap.value = texture;
         texture.wrapS = texture.wrapT = THREE.REPEAT_WRAPPING;
-
     });
 	
 	this.mesh = new THREE.Mesh(geomWaves, shader);
@@ -961,7 +966,7 @@ function loop(e){
 
 	renderer.render(scene, camera);
 
-	//boat.swayBoat();
+	boat.swayBoat();
 	sea.uniforms.uTime.value = e * 0.001;
 	requestAnimationFrame(loop);
 	update();
